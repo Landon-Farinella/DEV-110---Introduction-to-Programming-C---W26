448184e69df4c58b08689e47a16e9c1e1375f3b1
FarinellaForge <landonfarinella@gmail.com>
2026-01-13 00:30:09 -0600

    Complete Week 1: Hello GitHub assignment

--- modules/week-01-hello-github/starter/Program.cs
+++ modules/week-01-hello-github/starter/Program.cs
@@ -1,9 +1,12 @@
 namespace HelloGitHub;
 
+using System;
+
 public class Program
 {
     public static void Main(string[] args)
     {
         // TODO: Write code that outputs "Hello, GitHub!" to the console
+        Console.WriteLine("Hello, GitHub!");
     }
 }
